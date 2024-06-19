ba16783b9582c6df0993ba88a6abe0a6878f921e
Chris Hyer <hyer.chris@gmail.com>
2024-06-19 15:44:17 -0700

    Refactor error-page component and add return to login button

--- src/error-page.jsx
+++ src/error-page.jsx
@@ -1,15 +1,19 @@
-import { Box, Text, Title, Blockquote } from "@mantine/core";
-import { useRouteError } from "react-router-dom";
+import { Box, Text, Title, Blockquote, Button } from "@mantine/core";
+import { useNavigate, useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
-    const error = useRouteError();
-    console.error(error);
+  const error = useRouteError();
+  const navigate = useNavigate();
+  console.error(error);
 
-    return (
-        <Box id="error-page">
-            <Title order={1}>Oops!</Title>
-            <Text>Sorry, an unexpected error has occurred.</Text>
-            <Blockquote color="red" cite="- Error Team">{error.statusText || error.message}</Blockquote>
-        </Box>
-    );
+  return (
+    <Box id="error-page">
+      <Title order={1}>Oops!</Title>
+      <Text>Sorry, an unexpected error has occurred.</Text>
+      <Blockquote color="red" cite="- Error Team">
+        {error.statusText || error.message}
+      </Blockquote>
+      <Button onClick={() => navigate("/")}>Return to Login</Button>
+    </Box>
+  );
 }
