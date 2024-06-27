bbc9c1c7755527b2b07255f0826de8cc0a73d19c
Chris Hyer <hyer.chris@gmail.com>
2024-06-27 07:40:46 -0700

    fixed error notification not turning red properly

--- src/components/Admin/Login/Login.jsx
+++ src/components/Admin/Login/Login.jsx
@@ -140,7 +140,7 @@ export default function Login() {
       {loginStatus && (
         <Notification
           color={
-            loginStatus.includes("Error") || loginStatus.includes("Failed")
+            loginStatus.includes("Error") || loginStatus.includes("failed")
               ? "red"
               : loginStatus.includes("successful")
               ? "green"
